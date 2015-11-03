a032b86da84010237f9424abfb5a206cd189ce9e
DMSys <open.dmsys@gmail.com>
2015-11-03 23:10:30 +0200

    no message

--- PostStore/Areas/Api/Controllers/PostController.cs
+++ PostStore/Areas/Api/Controllers/PostController.cs
@@ -35,9 +35,41 @@ namespace PostStore.Areas.Api.Controllers
             using (System.IO.FileStream f2 = new System.IO.FileStream(fileName, System.IO.FileMode.Create))
             {
                 byte[] jsonArray = stream1.ToArray();
-                //f2.Write(jsonArray, 0, jsonArray.Length);
-            
-                using (System.IO.Compression.GZipStream gz = new System.IO.Compression.GZipStream(f2, System.IO.Compression.CompressionMode.Compress, false))
+                 f2.Write(jsonArray, 0, jsonArray.Length);
+                    /*
+                using (System.IO.Compression.GZipStream gz =
+                    new System.IO.Compression.GZipStream(f2, System.IO.Compression.CompressionMode.Compress))
+                {
+                    gz.Write(jsonArray, 0, jsonArray.Length);
+                }
+                */
+            }
+
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+
+        // GET: Api/Post/Add3
+        public JsonResult Add3()
+        {
+            PostModel model = new PostModel();
+            model.PDate = DateTime.Now;
+
+            string fileName = Server.MapPath("~/App_Data/test3.json");
+            model.PText = fileName;
+
+            System.Runtime.Serialization.Json.DataContractJsonSerializer ser =
+                new System.Runtime.Serialization.Json.DataContractJsonSerializer(typeof(PostModel));
+
+            System.IO.MemoryStream stream1 = new System.IO.MemoryStream();
+
+            ser.WriteObject(stream1, model);
+
+            using (System.IO.FileStream f2 = new System.IO.FileStream(fileName, System.IO.FileMode.Create))
+            {
+                byte[] jsonArray = stream1.ToArray();
+
+                using (System.IO.Compression.GZipStream gz =
+                    new System.IO.Compression.GZipStream(f2, System.IO.Compression.CompressionMode.Compress))
                 {
                     gz.Write(jsonArray, 0, jsonArray.Length);
                 }
--- PostStore/Controllers/HomeController.cs
+++ PostStore/Controllers/HomeController.cs
@@ -13,5 +13,44 @@ namespace PostStore.Controllers
         {
             return View();
         }
+
+        // GET: Home/GZPost
+        public FileResult GZPost(int id = 0)
+        {
+            if (id <= 0)
+            { return null; }
+            else
+            {
+                string fileName = GetFilePath(id);
+                if (System.IO.File.Exists(fileName))
+                {
+                    System.IO.MemoryStream postMemory = new System.IO.MemoryStream();
+                    using (System.IO.FileStream postFile = new System.IO.FileStream(fileName, System.IO.FileMode.Open))
+                    {
+                        postFile.CopyTo(postMemory);
+                        postFile.Close();
+                    }
+                    postMemory.Position = 0;
+                    FileStreamResult model = new FileStreamResult(postMemory, "application/json");
+                    Response.AppendHeader("Content-Encoding", "gzip");
+                    return model;
+                }
+                else
+                { return null; }
+            }
+        }
+
+        private string GetFilePath(int id)
+        {
+            string fileId = id.ToString();
+            string fileName = "";
+            while( fileId.Length > 3 )
+            {
+                fileName += "/" + fileId.Substring(0, 3);
+                fileId = fileId.Substring(3, fileId.Length - 3);
+            }
+            fileName += "/" + fileId;
+            return Server.MapPath("~/App_Data" + fileName + ".json");
+        }
     }
 }
