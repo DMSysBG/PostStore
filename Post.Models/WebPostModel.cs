ecb2b800254db2a2caefd39a1a7636d01445804b
DMSys <open.dmsys@gmail.com>
2015-11-08 19:13:25 +0200

    no message

---
+++ Post.Models/WebPostModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post.Models
+{
+    public class WebPostModel
+    {
+        public string PTitle { get; set; }
+
+        public string PText { get; set; }
+
+        public string PLink { get; set; }
+
+        public string PImage { get; set; }
+
+        public string PDate { get; set; }
+
+        public decimal PPrice { get; set; }
+    }
+}
--- PostStore/Areas/Api/Controllers/PostController.cs
+++ PostStore/Areas/Api/Controllers/PostController.cs
@@ -16,36 +16,44 @@ namespace PostStore.Areas.Api.Controllers
         }
 
         // GET: Api/Post/Add
-        public JsonResult Add()
+        public JsonResult Add(string model)
         {
-            PostModel model = new PostModel();
-            model.PDate = DateTime.Now;
+            string fileName = Server.MapPath("~/App_Data/4.json");
+            PostModel mPost = DMSys.Systems.ObjectJsonSerializer.Deserialize<PostModel>(model);
 
-            string fileName = Server.MapPath("~/App_Data/test.json");
-            model.PText = fileName;
+            SaveWebPost(fileName, mPost);
 
+            return Json(new JsonResponseModel() { Status = 0, Message = "Successfully completed" }
+                , JsonRequestBehavior.AllowGet);
+        }
 
-              System.Runtime.Serialization.Json.DataContractJsonSerializer ser = 
-                  new System.Runtime.Serialization.Json.DataContractJsonSerializer(typeof(PostModel));
-  
-            System.IO.MemoryStream stream1 = new System.IO.MemoryStream();
+        private void SaveWebPost(string fileName, PostModel model)
+        {
+            WebPostModel wPost = new WebPostModel()
+            {
+                PTitle = model.PTitle,
+                PText = model.PText,
+                PLink = model.PLink,
+                PImage = model.PImage,
+                PDate = model.PDate.ToString("yyyy-MM-ddTHH:mm:ss"),
+                PPrice = model.PPrice
+            };
 
-            ser.WriteObject(stream1, model);
-            
+            System.IO.MemoryStream msPost = new System.IO.MemoryStream();
+            System.Runtime.Serialization.Json.DataContractJsonSerializer dcJsonPost =
+                new System.Runtime.Serialization.Json.DataContractJsonSerializer(typeof(WebPostModel));
+            dcJsonPost.WriteObject(msPost, wPost);
+
             using (System.IO.FileStream f2 = new System.IO.FileStream(fileName, System.IO.FileMode.Create))
             {
-                byte[] jsonArray = stream1.ToArray();
-                 f2.Write(jsonArray, 0, jsonArray.Length);
-                    /*
+                byte[] jsonArray = msPost.ToArray();
+
                 using (System.IO.Compression.GZipStream gz =
                     new System.IO.Compression.GZipStream(f2, System.IO.Compression.CompressionMode.Compress))
                 {
                     gz.Write(jsonArray, 0, jsonArray.Length);
                 }
-                */
             }
-
-            return Json(model, JsonRequestBehavior.AllowGet);
         }
 
         // GET: Api/Post/Add3
